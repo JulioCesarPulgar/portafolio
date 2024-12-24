45a9286922337b9b0466e342f417139341584349
JulioCesarPulgar <pulgarjuliocesar@gmail.com>
2024-12-24 00:54:26 -0400

    Inicio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. Gracias por contactarme!');
+    // Aquí podrías añadir código para enviar el formulario a un servidor
+});
